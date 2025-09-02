f47a39a470907992a3139d50f5bc9b07dc154016
dpvp-cds <dpvp.cds@emcotic.com>
2025-09-01 21:00:32 -0500

    enviar-reporte-personal.js

--- api/enviar-reporte-personal.js
+++ api/enviar-reporte-personal.js
@@ -97,7 +97,7 @@ export default async function handler(request, response) {
     }
     try {
         const datosCompletos = request.body;
-        const docRef = await db.collection('reportes-personales').add({ ...datosCompletos, fecha: admin.firestore.Timestamp.now() });
+        const docRef = await db.collection('reportes-personales').add({ ...datosCompletos, fecha: new Date().toISOString() });
         const pdfBuffer = await crearPDF(datosCompletos);
         const resend = new Resend(process.env.RESEND2_API_KEY);
         
@@ -117,5 +117,6 @@ export default async function handler(request, response) {
         response.status(500).json({ message: 'Error interno del servidor', error: error.message });
     }
 }
+
 
 
